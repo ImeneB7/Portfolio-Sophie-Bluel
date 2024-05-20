ca219dc0c81c3a786e943394170c1438e3da51af
ImeneB7 <imene.bel74@gmail.com>
2024-05-20 15:02:35 +0200

    ajout de la div filters et création des boutons en js

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", () =>
+/** WORKS **/
+
 fetch("http://localhost:5678/api/works")
     .then(res => res.json())                        
     .then(data => {
@@ -15,20 +18,40 @@ fetch("http://localhost:5678/api/works")
         gallery.appendChild(figure);
     })
 
-})
+}))
 
 
+/** CATEGORIES **/
+
+const categories = ["objets", "appartements", "hôtels & restaurants"];
+
+document.addEventListener("DOMContentLoaded", () => {
 fetch("http://localhost:5678/api/categories")
     .then(res => res.json())
-    .then(data => {
-        data.forEach(categories=> {
-            
-        })
+    .then((categories) => {
+        console.log("Données reçues par l'API:", categories)
+        const filter = document.querySelector(".filters");
+        if (!filter) {
+            console.error("Les élément avec la classe 'filters' n'apparaissent pas dans le DOM");
+            return;
+        }
 
-        
+        const allBtn = document.createElement("button");
+        allBtn.innerText = "Tous";
+        allBtn.className = "category-btn";
+        filter.appendChild(allBtn);
+    
+
+        for (let i = 0; i < categories.length; i++){
+            const category = categories[i];
+            console.log(categories);
+
+            const filter = document.querySelector(".filters");
+            const button = document.createElement("button");
+            button.className = "category-btn";
+            button.textContent = category.name;
+            filter.appendChild(button);
+       }
+
+    })
 })
-
-
-
-
-
