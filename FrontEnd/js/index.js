67210c4e2042ede5a1e9cbd1e83f8e1d43b22d10
ImeneB7 <imene.bel74@gmail.com>
2024-05-22 09:59:05 +0200

    filtrage des categories et ajout de classe en css pour le bouton

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,5 +1,8 @@
-document.addEventListener("DOMContentLoaded", () =>
-/** WORKS **/
+document.addEventListener("DOMContentLoaded", () => {
+/*********************************** WORKS *******************************************/
+
+/** Récupération des données works et insertion dans le DOM + définition de la data-category*/
+
 
 fetch("http://localhost:5678/api/works")
     .then(res => res.json())                        
@@ -15,17 +18,18 @@ fetch("http://localhost:5678/api/works")
         figcaption.innerHTML = work.title;
         figure.appendChild(img)
         figure.appendChild(figcaption);
+        figure.setAttribute("data-category", work.categoryId);
         gallery.appendChild(figure);
     })
 
-}))
+})
 
 
-/** CATEGORIES **/
+/********************************** CATEGORIES ****************************************/
 
 const categories = ["objets", "appartements", "hôtels & restaurants"];
 
-document.addEventListener("DOMContentLoaded", () => {
+
 fetch("http://localhost:5678/api/categories")
     .then(res => res.json())
     .then((categories) => {
@@ -36,22 +40,62 @@ fetch("http://localhost:5678/api/categories")
             return;
         }
 
-        const allBtn = document.createElement("button");
+        /*** Création du bouton TOUS */
+
+        const allBtn = document.createElement("button");        
         allBtn.innerText = "Tous";
         allBtn.className = "category-btn";
         filter.appendChild(allBtn);
-    
 
-        for (let i = 0; i < categories.length; i++){
-            const category = categories[i];
-            console.log(categories);
+        allBtn.addEventListener("click", () =>{
+            console.log(`Button ${"Tous"} clicked`)
+            document.querySelectorAll(".gallery figure").forEach(figure => {
+                figure.style.display = "block";
+            })
+        })
 
+        /*** Création des autres boutons + data-category pour identifier la categorie */
+
+        categories.forEach(category => {
             const filter = document.querySelector(".filters");
             const button = document.createElement("button");
             button.className = "category-btn";
             button.textContent = category.name;
+            button.setAttribute("data-category-id", category.id)
             filter.appendChild(button);
-       }
+            
+            button.addEventListener("click", () =>{
+                const selectedCategory = category.id;
+                document.querySelectorAll(".gallery figure").forEach(figure => {
+                    if (figure.getAttribute("data-category") == selectedCategory) {
+                        figure.style.display = "block";
+                    } else {
+                        figure.style.display= "none";
+                    }
+                })
+                console.log(`Button ${category.name} clicked`)
+            })
+       })
+
+
+       /********************* TRIER PAR CATEGORIES **********************/
+
+       
+       categoryFilter();
 
     })
+
+        function categoryFilter() {
+            const buttons = document.querySelectorAll(".filters button")
+            console.log(buttons)
+            buttons.forEach((button, index) => {
+                button.addEventListener("click",() => {
+                    console.log(button);  
+                    buttons.forEach(button => button.classList.remove("btn-selected"))
+                    button.classList.add("btn-selected")
+                });   
+            });
+        }
 })
+
+
