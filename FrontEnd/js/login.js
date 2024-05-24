d8277aab862dd5f3ebe906feef6b296b1aa88df9
ImeneB7 <imene.bel74@gmail.com>
2024-05-24 15:58:51 +0200

    création de la page html login et du script login

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,52 @@
+
+document.addEventListener('DOMContentLoaded',() => {
+    const loginLink = document.getElementById('login');
+    const form = document.querySelector("form"); 
+
+    form.addEventListener('submit',  (e) => {
+        e.preventDefault();
+
+        /*** VARIABLES ***/
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const errorMessage = document.querySelector(".error-message");
+
+        console.log("email:", email);
+        console.log("password:", password);
+
+        /*** Envoi des données au serveur  à l'aide de la variable ***/
+        let data = {
+            email: email,
+            password: password,
+        };
+
+        fetch('http://localhost:5678/api/users/login', {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json" 
+            },
+            body:  JSON.stringify(data)
+    })
+    .then((res) => res.json()) 
+    .then((responseData) => {
+        console.log("response data:", responseData);
+        if (responseData.token){
+            localStorage.setItem("token", responseData.token)
+            window.location="index.html"
+        } else {
+            errorMessage.textContent = "Erreur dans l'identifiant ou le mot de passe";
+            errorMessage.classList.add("error-message")
+        }
+    })
+    .catch(error => {
+        console.error('Erreur', error)
+    })
+})
+
+
+
+
+
+
+})
